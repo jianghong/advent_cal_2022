3a69458245742de79b3b69b287834cd85e4d8260
Jackson Hong <jackson.hong@shopify.com>
2022-12-02 09:28:44 -0500

    Day 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -9,8 +9,31 @@ enum RPSMove {
     Unknown,
 }
 
+#[derive(Debug, Clone, Copy)]
+enum RPSOutcome {
+    Loss,
+    Draw,
+    Win
+}
+
+#[derive(Debug, Clone, Copy)]
 struct Play {
     value: RPSMove,
+}
+
+struct Outcome {
+    value: RPSOutcome
+}
+
+impl Outcome {
+    fn build(value: String) -> Outcome {
+        match value.as_str() {
+            "X" => Outcome { value: RPSOutcome::Loss },
+            "Y" => Outcome { value: RPSOutcome::Draw },
+            "Z" => Outcome { value: RPSOutcome::Win },
+            _ => panic!("Unknown outcome"),
+        }
+    }
 }
 
 impl Play {
@@ -44,15 +67,30 @@ impl RPS {
             _ => panic!("unknown moves {:?} {:?}", play1.value, play2.value),
         }
     }
+
+    fn calculate_points2(play1: Play, outcome: Outcome) -> i32 {
+        match (play1.value, outcome.value) {
+            (RPSMove::Paper, RPSOutcome::Loss) => 1,
+            (RPSMove::Scissors, RPSOutcome::Loss) => 2,
+            (RPSMove::Rock, RPSOutcome::Loss) => 3,
+            (RPSMove::Rock, RPSOutcome::Draw) => 4,
+            (RPSMove::Paper, RPSOutcome::Draw) => 5,
+            (RPSMove::Scissors, RPSOutcome::Draw) => 6,
+            (RPSMove::Scissors, RPSOutcome::Win) => 7,
+            (RPSMove::Rock, RPSOutcome::Win) => 8,
+            (RPSMove::Paper, RPSOutcome::Win) => 9,
+            _ => panic!("unknown moves {:?} {:?}", play1.value, outcome.value),
+        }
+    }
 }
 
 fn main() {
-    // Open the file in read-only mode (ignoring errors).
     let filename = "input.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
     let mut sum_points = 0;
+    let mut sum_points2 = 0;
     for (_, line) in reader.lines().enumerate() {
         let line = line.unwrap(); // Ignore errors.
         
@@ -62,9 +100,14 @@ fn main() {
 
         let play0 = Play::build(column0.to_string());
         let play1 = Play::build(column1.to_string());
-        let points = RPS::calculate_points(play0, play1);
+        let points = RPS::calculate_points(play0.clone(), play1);
         sum_points += points;
+        
+        let outcome = Outcome::build(column1.to_string());
+        let points2 = RPS::calculate_points2(play0, outcome);
+        sum_points2 += points2;
     }
 
     println!("Sum of points: {}", sum_points);
+    println!("Sum of points2: {}", sum_points2);
 }
