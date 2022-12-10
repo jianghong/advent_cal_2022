1c313240f17755c986d514d3079fc8d1cde3a4d4
Jackson Hong <hijackson12@icloud.com>
2022-12-10 13:01:49 -0500

    day 6 part 2

--- rs/day_06/src/main.rs
+++ rs/day_06/src/main.rs
@@ -1,35 +1,61 @@
 use std::process::exit;
 
-struct FourItemStack {
-    stack: [char; 4]
+struct NItemStack {
+    stack: Vec<char>,
+    n: usize
 }
 
-impl FourItemStack {
-    fn new() -> FourItemStack {
-        FourItemStack {
-            stack: [' ', ' ', ' ', ' ']
+impl NItemStack {
+    fn new(n: usize) -> NItemStack {
+        NItemStack {
+            stack: vec![' '; n],
+            n: n
         }
     }
 
     fn push(&mut self, c: char) {
-        self.stack[0] = self.stack[1];
-        self.stack[1] = self.stack[2];
-        self.stack[2] = self.stack[3];
-        self.stack[3] = c;
+        for i in 0..self.n-1 {
+            self.stack[i] = self.stack[i+1];
+        }
+        self.stack[self.n-1] = c;
     }
 
     fn has_duplicates(&self) -> bool {
-        self.stack[0] == self.stack[1] || self.stack[0] == self.stack[2] || self.stack[0] == self.stack[3] ||
-        self.stack[1] == self.stack[2] || self.stack[1] == self.stack[3] ||
-        self.stack[2] == self.stack[3]
+        for i in 0..self.n-1 {
+            for j in i+1..self.n {
+                if self.stack[i] == self.stack[j] {
+                    return true;
+                }
+            }
+        }
+        false
     }
 }
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let filename = args[1].clone();
+
+    part2(filename.clone());
+}
+
+fn part2(filename: String) {
+    let input = std::fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let mut fourteen_stack = NItemStack::new(14);
+    for (i, c) in input.chars().enumerate() {
+        fourteen_stack.push(c);
+        if (i > 2) && (!fourteen_stack.has_duplicates()) {
+            println!("Found start-of-message marker at the {}th character", i + 1);
+            exit(0);
+        }
+    }
+    println!("No start-of-message marker found");
+}
+
+fn part1(filename: String) {
     let input = std::fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    let mut four_stack = FourItemStack::new();
+    let mut four_stack = NItemStack::new(4);
     
     // iterate input with index
     for (i, c) in input.chars().enumerate() {
