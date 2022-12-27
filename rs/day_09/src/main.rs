c08a860acff3e1b6c28254d78d9d52e83cf4197c
Jackson Hong <hijackson12@icloud.com>
2022-12-27 16:26:30 -0500

    progress on day 9 part 2

--- rs/day_09/src/main.rs
+++ rs/day_09/src/main.rs
@@ -1,5 +1,42 @@
 use std::io::{BufReader, BufRead};
 
+#[derive(Debug)]
+struct Segment {
+    next: Option<Box<Segment>>,
+    pos: Position,
+}
+
+impl Segment {
+    fn is_tail(&self) -> bool {
+        self.next.is_none()
+    }
+}
+
+#[derive(Debug)]
+struct Rope {
+    head: Option<Box<Segment>>
+}
+
+impl Rope {
+    fn new(size: u64) -> Rope {
+        let mut rope = Rope {
+            head: None,
+        };
+        rope.push_n(size);
+        rope
+    }
+
+    fn push_n(&mut self, n: u64) {
+        for _ in 0..n {
+            let new_segment = Box::new(Segment {
+                next: self.head.take(),
+                pos: Position { x: 0, y: 0 },
+            });
+            self.head = Some(new_segment);
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 struct Position {
     x: i64,
@@ -7,109 +44,64 @@ struct Position {
 }
 
 struct KnotTracker {
-    curr_head_pos: Position,
-    curr_tail_pos: Position,
     known_tail_positions: Vec<Position>,
+    rope: Rope,
 }
 
 impl KnotTracker {
     fn new() -> KnotTracker {
         KnotTracker {
-            curr_head_pos: Position { x: 0, y: 0 },
-            curr_tail_pos: Position { x: 0, y: 0 },
-            known_tail_positions: vec![Position { x: 0, y: 0 }]
+            known_tail_positions: vec![Position { x: 0, y: 0 }],
+            rope: Rope::new(10)
         }
     }
 
-    fn add_current_tail_position(&mut self) {
-        if !self.known_tail_positions.contains(&self.curr_tail_pos) {
-            self.known_tail_positions.push(self.curr_tail_pos);
+    fn add_current_tail_position(&mut self, pos: Position) {
+        if !self.known_tail_positions.contains(&pos) {
+            self.known_tail_positions.push(pos);
         }
     }
 
     fn move_head(&mut self, direction: &str, steps: i64) {
         match direction {
             "R" => self.move_head_right(steps),
-            "L" => self.move_head_left(steps),
-            "U" => self.move_head_up(steps),
-            "D" => self.move_head_down(steps),
+            // "L" => self.move_head_left(steps),
+            // "U" => self.move_head_up(steps),
+            // "D" => self.move_head_down(steps),
             _ => panic!("Invalid direction"),
         }
-
-        println!("Current head position: {:?}", self.curr_head_pos);
-        println!("Current tail position: {:?}", self.curr_tail_pos);
     }
 
     fn move_head_right(&mut self, steps: i64) {
         for _ in 0..steps {
-            self.curr_head_pos = Position { x: self.curr_head_pos.x + 1, y: self.curr_head_pos.y };
-            let dist = self.distance_of_head_and_tail(); 
+            let mut curr_segment = self.rope.head.as_mut().unwrap();
+            curr_segment.pos.x += 1;
             
-            if dist.0 == 2 {
-                self.curr_tail_pos = Position {
-                    x: self.curr_tail_pos.x + 1,
-                    y: self.curr_tail_pos.y + dist.1
-                };
-                self.add_current_tail_position();
+            while curr_segment.next.is_some() {
+                let next_segment = curr_segment.next.as_mut().unwrap();
+                let dist = distance_of_curr_and_next(&curr_segment.pos, &next_segment.pos);
+                
+                if dist.0 == 2 {
+                    next_segment.pos.x += 1;
+                    next_segment.pos.y += dist.1;
+                }
+
+                curr_segment = next_segment;
             }
         }
     }
 
-    fn move_head_up(&mut self, steps: i64) {
-        for _ in 0..steps {
-            self.curr_head_pos = Position { x: self.curr_head_pos.x, y: self.curr_head_pos.y + 1 };
-            let dist = self.distance_of_head_and_tail(); 
+}
 
-            if dist.1 == 2 {
-                self.curr_tail_pos = Position {
-                    x: self.curr_tail_pos.x + dist.0,
-                    y: self.curr_tail_pos.y + 1
-                };
-                self.add_current_tail_position();
-            }
-        }
-    }
+fn distance_of_curr_and_next(curr_pos: &Position, next_pos: &Position) -> (i64, i64) {
+    let x_diff = curr_pos.x - next_pos.x;
+    let y_diff = curr_pos.y - next_pos.y;
 
-    fn move_head_left(&mut self, steps: i64) {
-        for _ in 0..steps {
-            self.curr_head_pos = Position { x: self.curr_head_pos.x - 1, y: self.curr_head_pos.y };
-            let dist = self.distance_of_head_and_tail(); 
-
-            if dist.0 == -2 {
-                self.curr_tail_pos = Position {
-                    x: self.curr_tail_pos.x - 1,
-                    y: self.curr_tail_pos.y + dist.1
-                };
-                self.add_current_tail_position();
-            }
-        } 
-    }
-
-    fn move_head_down(&mut self, steps: i64) {
-        for _ in 0..steps {
-            self.curr_head_pos = Position { x: self.curr_head_pos.x, y: self.curr_head_pos.y - 1 };
-            let dist = self.distance_of_head_and_tail(); 
-
-            if dist.1 == -2 {
-                self.curr_tail_pos = Position {
-                    x: self.curr_tail_pos.x + dist.0,
-                    y: self.curr_tail_pos.y - 1
-                };
-                self.add_current_tail_position();
-            }
-        }
-    }
-
-    fn distance_of_head_and_tail(&self) -> (i64, i64) {
-        let x_diff = self.curr_head_pos.x - self.curr_tail_pos.x;
-        let y_diff = self.curr_head_pos.y - self.curr_tail_pos.y;
-
-        (x_diff, y_diff)
-    }
+    (x_diff, y_diff)
 }
 
 fn main() {
-    let file = std::fs::File::open("input.txt").unwrap();
+    let file = std::fs::File::open("sample.txt").unwrap();
     let reader = BufReader::new(file);
 
     let mut knot_tracker = KnotTracker::new();
@@ -123,6 +115,5 @@ fn main() {
         knot_tracker.move_head(direction, steps);
     }
 
-    println!("Known tail positions: {:?}", knot_tracker.known_tail_positions);
-    println!("num tail positions: {}", knot_tracker.known_tail_positions.len());
+    println!("{:?}", knot_tracker.rope);
 }
