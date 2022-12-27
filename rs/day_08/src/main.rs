1f370d23bc7ff72709e77d04cde3465572831641
Jackson Hong <hijackson12@icloud.com>
2022-12-27 00:13:35 -0500

    day 8 part 1

---
+++ rs/day_08/src/main.rs
@@ -0,0 +1,119 @@
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+struct Map {
+    grid: Vec<Vec<i8>>,
+}
+
+impl Map {
+    fn add_row(&mut self, row: Vec<i8>) {
+        self.grid.push(row);
+    }
+
+    fn num_cols(&self) -> i64 {
+        self.grid[0].len() as i64
+    }
+
+    fn num_rows(&self) -> i64 {
+        self.grid.len() as i64
+    }
+
+    fn calculate_trees_from_edges(&self) -> i64 {
+        (self.num_rows() * 2) + (self.num_cols() * 2) - 4
+    }
+
+    fn calculate_interior_trees(&self) -> i64 {
+        let starting_x = 1;
+        let starting_y = 1;
+        let ending_x = self.num_rows() - 1;
+        let ending_y = self.num_cols() - 1;
+        let mut visible_trees = 0;
+        for x in starting_x..ending_x {
+            for y in starting_y..ending_y {
+                if self.check_tree_visible(x, y) {
+                    visible_trees += 1;
+                }
+            }
+        }
+
+        return visible_trees
+    }
+
+    fn check_tree_visible(&self, x: i64 ,y: i64) -> bool {
+        self.check_visible_from_top(x, y) ||
+        self.check_visible_from_right(x, y) ||
+        self.check_visible_from_bottom(x, y) ||
+        self.check_visible_from_left(x, y)
+    }
+
+    fn check_visible_from_top(&self, x: i64, y: i64) -> bool {
+        let mut x = x as usize;
+        let y = y as usize;
+        let tree_height = self.grid[x][y];
+        while x > 0 {
+            if self.grid[x - 1][y] >= tree_height {
+                return false;
+            }
+            x -= 1;
+        }
+        return true
+    }
+
+    fn check_visible_from_right(&self, x: i64, y: i64) -> bool {
+        let x = x as usize;
+        let mut y = y as usize;
+        let tree_height = self.grid[x][y];
+        while y < self.num_cols() as usize - 1 {
+            if self.grid[x][y + 1] >= tree_height {
+                return false;
+            }
+            y += 1;
+        }
+        return true
+    }
+
+    fn check_visible_from_bottom(&self, x: i64, y: i64) -> bool {
+        let mut x = x as usize;
+        let y = y as usize;
+        let tree_height = self.grid[x][y];
+        while x < self.num_rows() as usize - 1 {
+            if self.grid[x + 1][y] >= tree_height {
+                return false;
+            }
+            x += 1;
+        }
+        true
+    }
+
+    fn check_visible_from_left(&self, x: i64, y: i64) -> bool {
+        let x = x as usize;
+        let mut y = y as usize;
+        let tree_height = self.grid[x][y];
+        while y > 0 {
+            if self.grid[x][y - 1] >= tree_height {
+                return false;
+            }
+            y -= 1;
+        }
+        true
+    }
+
+    fn total_visible_trees(&self) -> i64 {
+        self.calculate_trees_from_edges() + self.calculate_interior_trees()
+    }
+}
+
+fn main() {
+    let file = std::fs::File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut map = Map { grid: Vec::new() };
+    
+
+    for (_, line) in reader.lines().enumerate(){
+        let line = line.unwrap();
+        let digits: Vec<i8> = line.chars().map(|c| c.to_digit(10).unwrap() as i8).collect();
+        map.add_row(digits);
+    }
+
+    println!("Total visible trees: {}", map.total_visible_trees());
+}
