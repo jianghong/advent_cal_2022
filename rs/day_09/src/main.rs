6827ea66467b70306eb9a218491617460ae34c66
Jackson Hong <hijackson12@icloud.com>
2022-12-28 00:16:05 -0500

    done day 9 part 2

--- rs/day_09/src/main.rs
+++ rs/day_09/src/main.rs
@@ -34,9 +34,8 @@ impl Rope {
 
 impl fmt::Display for Rope {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // create 100x100 grid of "." char
         let offset_x = 20;
-        let offset_y = 80;
+        let offset_y = 20;
         let mut grid = vec![vec![".".to_string(); 100]; 100];
 
         let mut curr_segment = self.head.as_ref().unwrap();
@@ -44,10 +43,17 @@ impl fmt::Display for Rope {
         let mut id = 1;
         while curr_segment.next.is_some() {
             curr_segment = curr_segment.next.as_ref().unwrap();
-            grid[(curr_segment.pos.y + offset_y) as usize][(curr_segment.pos.x + offset_x) as usize] = id.to_string();
+            if grid[offset_y as usize][offset_x as usize] == ".".to_string() {
+                grid[(curr_segment.pos.y + offset_y) as usize][(curr_segment.pos.x + offset_x) as usize] = id.to_string();
+            }
             id += 1;
         }
-        grid[offset_y as usize][offset_x as usize] = "S".to_string();
+        
+        if grid[offset_y as usize][offset_x as usize] == ".".to_string() {
+            grid[offset_y as usize][offset_x as usize] = "S".to_string();
+        }
+
+        grid.reverse();
         for row in grid {
             for c in row {
                 write!(f, "{}", c)?;
@@ -82,96 +88,46 @@ impl KnotTracker {
     }
 
     fn move_head(&mut self, rope: &mut Rope, direction: &str, steps: i64) {
-        match direction {
-            "R" => self.move_head_right(rope, steps),
-            "L" => self.move_head_left(rope, steps),
-            "U" => self.move_head_up(rope, steps),
-            "D" => self.move_head_down(rope, steps),
-            _ => panic!("Invalid direction"),
-        }
-        // println!("head pos: {:?}", rope.head.as_ref().unwrap().pos);
-    }
-
-    fn move_head_right(&mut self, rope: &mut Rope, steps: i64) {
         for _ in 0..steps {
             let mut curr_segment = rope.head.as_mut().unwrap();
-            curr_segment.pos.x += 1;
-            
+            match direction {
+                "R" => curr_segment.pos.x += 1,
+                "L" => curr_segment.pos.x -=1,
+                "U" => curr_segment.pos.y += 1,
+                "D" => curr_segment.pos.y -= 1,
+                _ => panic!("Invalid direction"),
+            }
+
             while curr_segment.next.is_some() {
                 let next_segment = curr_segment.next.as_mut().unwrap();
                 let dist = distance_of_curr_and_next(&curr_segment.pos, &next_segment.pos);
                 
-                if dist.0 == 2 {
+                if dist == (2, 0) {
+                    next_segment.pos.x += 1
+                } else if dist == (0, 2) {
+                    next_segment.pos.y += 1
+                } else if dist.0 + dist.1 >= 3 {
                     next_segment.pos.x += 1;
-                    next_segment.pos.y += dist.1;
-                }
-
-                curr_segment = next_segment;
-            }
-            self.add_current_tail_position(curr_segment.pos);
-        }
-    }
-
-    fn move_head_up(&mut self, rope: &mut Rope, steps: i64) {
-        for _ in 0..steps {
-            let mut curr_segment = rope.head.as_mut().unwrap();
-            curr_segment.pos.y += 1;
-            
-            while curr_segment.next.is_some() {
-                let next_segment = curr_segment.next.as_mut().unwrap();
-                let dist = distance_of_curr_and_next(&curr_segment.pos, &next_segment.pos);
-                
-                if dist.1 == 2 {
                     next_segment.pos.y += 1;
-                    next_segment.pos.x += dist.0;
-                }
-
-                curr_segment = next_segment;
-            }
-            self.add_current_tail_position(curr_segment.pos);
-        }
-    }
-
-    fn move_head_left(&mut self, rope: &mut Rope, steps: i64) {
-        for _ in 0..steps {
-            let mut curr_segment = rope.head.as_mut().unwrap();
-            curr_segment.pos.x -= 1;
-            
-            while curr_segment.next.is_some() {
-                let next_segment = curr_segment.next.as_mut().unwrap();
-                let dist = distance_of_curr_and_next(&curr_segment.pos, &next_segment.pos);
-                
-                if dist.0 == -2 {
+                } else if dist == (-2, 0) {
                     next_segment.pos.x -= 1;
-                    next_segment.pos.y += dist.1;
-                }
-
-                curr_segment = next_segment;
-            }
-            self.add_current_tail_position(curr_segment.pos);
-        }
-    }
-
-    fn move_head_down(&mut self, rope: &mut Rope, steps: i64) {
-        for _ in 0..steps {
-            let mut curr_segment = rope.head.as_mut().unwrap();
-            curr_segment.pos.y -= 1;
-            
-            while curr_segment.next.is_some() {
-                let next_segment = curr_segment.next.as_mut().unwrap();
-                let dist = distance_of_curr_and_next(&curr_segment.pos, &next_segment.pos);
-                
-                if dist.1 == -2 {
+                } else if dist == (0, -2) {
                     next_segment.pos.y -= 1;
-                    next_segment.pos.x += dist.0;
+                } else if dist == (-2, 1) || dist == (-1, 2) || dist == (-2, 2) {
+                    next_segment.pos.x -= 1;
+                    next_segment.pos.y += 1;
+                } else if dist == (-1, -2 ) || dist == (-2, -1) || dist == (-2, -2) {
+                    next_segment.pos.x -= 1;
+                    next_segment.pos.y -= 1;
+                } else if dist == (2, -1) || dist == (1, -2) || dist == (2, -2) {
+                    next_segment.pos.x += 1;
+                    next_segment.pos.y -= 1;
                 }
-
                 curr_segment = next_segment;
             }
             self.add_current_tail_position(curr_segment.pos);
         }
     }
-
 }
 
 fn distance_of_curr_and_next(curr_pos: &Position, next_pos: &Position) -> (i64, i64) {
@@ -182,7 +138,7 @@ fn distance_of_curr_and_next(curr_pos: &Position, next_pos: &Position) -> (i64, 
 }
 
 fn main() {
-    let file = std::fs::File::open("sample.txt").unwrap();
+    let file = std::fs::File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
 
     let mut rope = Rope::new(10);
@@ -197,7 +153,7 @@ fn main() {
         knot_tracker.move_head(&mut rope, direction, steps);
     }
 
-    println!("{}", rope);
-    // println!("num of known tail positions {:?}", knot_tracker.known_tail_positions.len());
+    // println!("{}", rope);
+    println!("num of known tail positions {:?}", knot_tracker.known_tail_positions.len());
     // println!("known tail positions {:?}", knot_tracker.known_tail_positions);
 }
